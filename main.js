104008eb475d51718ddc90091bce99049388185f
Alexander Vetrov <notescape262@gmail.com>
2023-02-17 20:57:21 +0200

    ortographic camera

--- main.js
+++ main.js
@@ -13,8 +13,20 @@ const size = {
   height: 600,
 }
 
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height)
+// const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 100)
+const aspectRatio = size.width / size.height
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
+  0.1,
+  100
+)
+camera.position.x = 2
+camera.position.y = 2
 camera.position.z = 3
+camera.lookAt(cube.position)
 scene.add(camera)
 
 const canvas = document.querySelector('.webgl')
@@ -24,7 +36,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(size.width, size.height)
 
 const tick = () => {
-	
+	cube.rotation.y += 0.004
 	
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)

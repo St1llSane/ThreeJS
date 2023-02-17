d69cdff8fe15a3155b91bb5a02529f95686f6ae8
Alexander Vetrov <notescape262@gmail.com>
2023-02-18 00:22:57 +0200

    resizing on window resize

---
+++ main.js
@@ -0,0 +1,64 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import './style.css'
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 'yellow' })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// Resize
+window.addEventListener('resize', () => {
+  size.width = window.innerWidth
+  size.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = size.width / size.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(size.width, size.height)
+})
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  size.width / size.height,
+  0.1,
+  100
+)
+
+camera.position.x = 2
+camera.position.y = 1
+camera.position.z = 2
+camera.lookAt(cube.position)
+scene.add(camera)
+
+const canvas = document.querySelector('.webgl')
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.dampingFactor = 0.03
+controls.minDistance = 2
+controls.maxDistance = 8
+controls.target = cube.position
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+})
+renderer.setSize(size.width, size.height)
+
+const tick = () => {
+  controls.update()
+
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+tick()

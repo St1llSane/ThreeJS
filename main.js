0a9b584045598f2781ed3efb2799447e4e05e0c7
Alexander Vetrov <notescape262@gmail.com>
2023-02-17 19:21:34 +0200

    base animation by gsap

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 import './style.css'
 
 const scene = new THREE.Scene()
@@ -23,18 +24,24 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(size.width, size.height)
 
-const clock = new THREE.Clock()
+gsap.to(cube.position, { duration: .5, delay: 1, x: 2 })
+gsap.to(cube.position, { duration: 1, delay: 1.5, x: -2 })
+gsap.to(cube.position, { duration: .5, delay: 2.5, y: 1 })
+gsap.to(cube.position, { duration: .5, delay: 2.5, x: 0 })
+gsap.to(cube.position, { duration: 1, delay: 3, y: 0 })
+
+// const clock = new THREE.Clock()
 const tick = () => {
-	const elapsedTime = clock.getElapsedTime()
-	
+  // const elapsedTime = clock.getElapsedTime()
+
   // cube.rotation.y = elapsedTime * Math.PI * 2
 
-	// cube.position.y = Math.sin(elapsedTime)
-	// cube.position.x = Math.cos(elapsedTime)
+  // cube.position.y = Math.sin(elapsedTime)
+  // cube.position.x = Math.cos(elapsedTime)
 
-	camera.position.y = Math.sin(elapsedTime)
-	camera.position.x = Math.cos(elapsedTime)
-	camera.lookAt(cube.position)
+  // camera.position.y = Math.sin(elapsedTime)
+  // camera.position.x = Math.cos(elapsedTime)
+  // camera.lookAt(cube.position)
 
   renderer.render(scene, camera)
 

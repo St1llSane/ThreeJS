3b570a3146119f50c584b5cdb08941d97101b98c
Alexander Vetrov <notescape262@gmail.com>
2023-02-17 21:26:19 +0200

    handling cursor position

--- main.js
+++ main.js
@@ -13,18 +13,33 @@ const size = {
   height: 600,
 }
 
-// const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 100)
-const aspectRatio = size.width / size.height
-const camera = new THREE.OrthographicCamera(
-  -1 * aspectRatio,
-  1 * aspectRatio,
-  1,
-  -1,
+const cursor = {
+  x: 0,
+  y: 0,
+}
+
+window.addEventListener('mousemove', (e) => {
+  console.log(cursor.x)
+  console.log(cursor.y)
+  cursor.x = e.clientX / size.width - 0.5
+  cursor.y = e.clientY / size.height - 0.5
+})
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  size.width / size.height,
   0.1,
   100
 )
-camera.position.x = 2
-camera.position.y = 2
+// const aspectRatio = size.width / size.height
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// )
 camera.position.z = 3
 camera.lookAt(cube.position)
 scene.add(camera)
@@ -36,8 +51,10 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(size.width, size.height)
 
 const tick = () => {
-	cube.rotation.y += 0.004
-	
+  camera.position.x = cursor.x * 4
+  camera.position.y = -cursor.y * 4
+	camera.lookAt(cube.position)
+
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)
 }

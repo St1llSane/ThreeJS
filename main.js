247666fe3061ba62305f4f7f90b76df8e64eec80
Alexander Vetrov <notescape262@gmail.com>
2023-02-18 16:59:27 +0200

    control-panel by dat.GUI

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'dat.gui'
+import gsap from 'gsap'
 import './style.css'
+
+// Debug UI
+const gui = new dat.GUI({ closed: false, width: 340 })
+
+const params = {
+  cubeColor: 0x7255de,
+  spin: () => {
+    gsap.to(cube.rotation, { duration: 1, y: cube.rotation.y + Math.PI * 2 })
+  },
+}
 
 const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-
-const material = new THREE.MeshBasicMaterial({ color: 'yellow' })
+const material = new THREE.MeshBasicMaterial({ color: params.cubeColor })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
+
+// Debug UI
+gui.add(cube.position, 'x', -3, 3, 0.1).name('yellow cube X')
+gui.add(cube.position, 'y', -3, 3, 0.1).name('yellow cube Y')
+gui.add(cube.position, 'z', -3, 3, 0.1).name('yellow cube Z')
+gui.add(cube.rotation, 'y', -Math.PI * 2, Math.PI * 2, 0.1).name('yellow cube rotation X')
+gui.add(cube.rotation, 'x', -Math.PI * 2, Math.PI * 2, 0.1).name('yellow cube rotation Y')
+gui.add(cube.rotation, 'z', -Math.PI * 2, Math.PI * 2, 0.1).name('yellow cube rotation Z')
+
+gui.addColor(params, 'cubeColor').onChange(() => {
+  material.color.set(params.cubeColor)
+})
+
+gui.add(cube, 'visible').name('yellow cube visible')
+gui.add(cube.material, 'wireframe')
+
+gui.add(params, 'spin')
 
 const size = {
   width: window.innerWidth,
@@ -29,14 +57,6 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-window.addEventListener('dblclick', () => {
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen()
-  } else {
-    document.exitFullscreen()
-  }
-})
-
 const camera = new THREE.PerspectiveCamera(
   75,
   size.width / size.height,
@@ -45,7 +65,6 @@ const camera = new THREE.PerspectiveCamera(
 )
 
 camera.position.z = 3
-camera.lookAt(cube.position)
 scene.add(camera)
 
 const canvas = document.querySelector('.webgl')
@@ -56,7 +75,6 @@ controls.enableDamping = true
 controls.dampingFactor = 0.03
 controls.minDistance = 2
 controls.maxDistance = 8
-controls.target = cube.position
 
 const renderer = new THREE.WebGLRenderer({
   canvas,

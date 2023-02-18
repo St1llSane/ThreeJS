6161f0c2b09739bdbe3ffe9d4b398fe13d99ed7b
Alexander Vetrov <notescape262@gmail.com>
2023-02-18 19:36:43 +0200

    load textures

--- main.js
+++ main.js
@@ -5,19 +5,33 @@ import gsap from 'gsap'
 import './style.css'
 
 // Textures
-const image = new Image()
-const texture = new THREE.Texture(image)
-
-image.onload = () => {
-  texture.needsUpdate = true
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onStart = () => {
+	console.log('onStart');
+}
+loadingManager.onProgress = () => {
+	console.log('onProgress');
+}
+loadingManager.onLoad = () => {
+	console.log('onLoad');
+}
+loadingManager.onError = () => {
+	console.log('onError');
 }
 
-image.src = './textures/door/color.jpg'
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTexture = textureLoader.load('./textures/door/color.jpg')
+const alphaTexture = textureLoader.load('./textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('./textures/door/height.jpg')
+const normalTexture = textureLoader.load('./textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
 
 const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ map: texture })
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 

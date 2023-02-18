933e6c458c405d475b81e08ed719003b87b35c43
Alexander Vetrov <notescape262@gmail.com>
2023-02-18 15:05:17 +0200

    another way to create 50 triangles by BufferAttribute

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ import './style.css'
 const scene = new THREE.Scene()
 
 // Create BufferGeometry
-const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0])
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
-
 const geometry = new THREE.BufferGeometry()
+
+const count = 500
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 3
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
 geometry.setAttribute('position', positionsAttribute)
 
 const material = new THREE.MeshBasicMaterial({

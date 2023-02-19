20d1151c64c8b3f76d3a3d2b749183899db171f0
Alexander Vetrov <66486104+St1llSane@users.noreply.github.com>
2023-02-19 15:45:16 +0300

    base-scene

---
+++ threejs-base/main.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import './style.css'
+
+// Scene
+const scene = new THREE.Scene()
+
+// Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 'yellow' })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+// Resizing
+window.addEventListener('resize', () => {
+	sizes.width = window.innerWidth
+	sizes.height = window.innerHeight
+
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
+
+	renderer.setSize(sizes.width, sizes.height)
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.dampingFactor = 0.03
+controls.minDistance = 1
+controls.maxDistance = 10
+controls.update()
+
+// Animations
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  // cube.rotation.y = elapsedTime / 4
+
+  renderer.render(scene, camera)
+  controls.update()
+  window.requestAnimationFrame(tick)
+}
+tick()

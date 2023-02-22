c4116313f6b6740aa03b53442583b33fc64956c2
Alexander Vetrov <notescape262@gmail.com>
2023-02-22 22:07:56 +0200

    helpers

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper'
 import './style.css'
 
 // Scene
@@ -32,6 +33,28 @@ spotLight.position.set(0, 2, 3)
 scene.add(spotLight)
 scene.add(spotLight.target)
 spotLight.target.position.x = -0.75
+
+// Helpers
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  0.2
+)
+scene.add(directionalLightHelper)
+
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(
+  hemisphereLight,
+  0.2
+)
+scene.add(hemisphereLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaLightHelper)
 
 // Material
 const material = new THREE.MeshStandardMaterial()

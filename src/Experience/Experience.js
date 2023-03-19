99fb9d06414526cae9a8e1fdb0cd57284e890d8a
Alexander Vetrov <notescape262@gmail.com>
2023-03-19 21:28:18 +0200

    env debug and destroy method

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -22,11 +22,11 @@ export default class Experience {
     this.canvas = _canvas
 
     // Setup
-		this.debug = new Debug()
+    this.debug = new Debug()
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
-		this.resources = new Resources(sources)
+    this.resources = new Resources(sources)
     this.camera = new Camera()
     this.renderer = new Renderer()
     this.world = new World()
@@ -50,6 +50,34 @@ export default class Experience {
   update() {
     this.camera.update()
     this.renderer.update()
-		this.world.update()
+    this.world.update()
+  }
+
+  destroy() {
+    this.sizes.off('resize')
+    this.time.off('tick')
+
+    // Traverse the whole scene
+    this.scene.traverse((child) => {
+      // Test if it's a mesh
+      if (child instanceof THREE.Mesh) {
+        child.geometry.dispose()
+
+        // Loop through the material properties
+        for (const key in child.material) {
+          const value = child.material[key]
+
+          // Test if there is a dispose function
+          if (value && typeof value.dispose === 'function') {
+            value.dispose()
+          }
+        }
+      }
+    })
+
+    this.camera.controls.dispose()
+    this.renderer.instance.dispose()
+
+    if (this.debug.active) this.debug.ui.destroy()
   }
 }
